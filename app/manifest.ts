ca00b8c21d61d66a36aa2b1943a8b74bffdb4642
inclavetrendyshop-beep <inclavetrendyshop@gmail.com>
2026-09-25 16:47:05 +0200

    Create manifest.ts

---
+++ app/manifest.ts
@@ -0,0 +1,32 @@
+import { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "Dandy",
+    short_name: "Dandy",
+    description: "Conecta con hombres sofisticados cerca de ti",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#0d0d0d",
+    theme_color: "#0d0d0d",
+    icons: [
+      {
+        src: "/icons/icon-192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/icons/icon-512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+      {
+        src: "/icons/icon-512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable",
+      },
+    ],
+  };
+}
+
